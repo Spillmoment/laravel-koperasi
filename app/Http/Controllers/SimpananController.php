4cd4e3a6be9d6fadff8ab29a3fa89fe7cba59036
gunawan <deddygunawan98@gmail.com>
2020-10-22 21:29:23 +0700

    create controller simpanan

---
+++ app/Http/Controllers/SimpananController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Simpanan;
+use Illuminate\Http\Request;
+
+class SimpananController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Simpanan  $simpanan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Simpanan $simpanan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Simpanan  $simpanan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Simpanan $simpanan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Simpanan  $simpanan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Simpanan $simpanan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Simpanan  $simpanan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Simpanan $simpanan)
+    {
+        //
+    }
+}
