35c1344b19e9db127f30585bc2a80e25e0b4ea9d
gunawan <deddygunawan98@gmail.com>
2020-10-22 21:29:04 +0700

    create controller anggota

---
+++ app/Http/Controllers/AnggotaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Anggota;
+use Illuminate\Http\Request;
+
+class AnggotaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Anggota  $anggota
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Anggota $anggota)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Anggota  $anggota
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Anggota $anggota)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Anggota  $anggota
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Anggota $anggota)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Anggota  $anggota
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Anggota $anggota)
+    {
+        //
+    }
+}
