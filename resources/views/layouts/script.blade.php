1eebf63038bfa7a0a5bd284c68d42ceff7bc22fc
gunawan <deddygunawan98@gmail.com>
2020-10-26 07:45:04 +0700

    pilah template - script

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,37 @@
+<!-- Core -->
+<script src="{{ asset('vendor/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+<!-- Vendor JS -->
+<script src="{{ asset('vendor/onscreen/dist/on-screen.umd.min.js') }}"></script>
+
+<!-- Slider -->
+<script src="{{ asset('vendor/nouislider/distribute/nouislider.min.js') }}"></script>
+
+<!-- Jarallax -->
+<script src="{{ asset('vendor/jarallax/dist/jarallax.min.js') }}"></script>
+
+<!-- Smooth scroll -->
+<script src="{{ asset('vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js') }}"></script>
+
+<!-- Count up -->
+<script src="{{ asset('vendor/countup.js/dist/countUp.umd.js') }}"></script>
+
+<!-- Notyf -->
+<script src="{{ asset('vendor/notyf/notyf.min.js') }}"></script>
+
+<!-- Charts -->
+<script src="{{ asset('vendor/chartist/dist/chartist.min.js') }}"></script>
+<script src="{{ asset('vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') }}"></script>
+
+<!-- Datepicker -->
+<script src="{{ asset('vendor/vanillajs-datepicker/dist/js/datepicker.min.js') }}"></script>
+
+<!-- Simplebar -->
+<script src="{{ asset('vendor/simplebar/dist/simplebar.min.js') }}"></script>
+
+<!-- Github buttons -->
+<script async defer src="https://buttons.github.io/buttons.js')"></script>
+
+<!-- Volt JS -->
+<script src="{{ asset('assets/js/volt.js') }}"></script>
