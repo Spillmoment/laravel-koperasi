b54236663676898b6f4682d7f1a2981239afc790
ackerman <do.crazy192@gmail.com>
2020-11-09 10:25:20 +0700

    add style blade

---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,9 @@
+    
+    <!-- Fontawesome -->
+    <link type="text/css" href="{{ asset('vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+
+    <!-- Notyf -->
+    <link type="text/css" href="{{ asset('vendor/notyf/notyf.min.css') }}" rel="stylesheet">
+
+    <!-- Volt CSS -->
+    <link type="text/css" href="{{ asset('css/volt.css') }}" rel="stylesheet">
