81eb5239ac3690af425648cba3f5ed7f7b221be8
ackerman <do.crazy192@gmail.com>
2020-11-10 21:04:34 +0700

    implement prefix & middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,18 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\{Route, Auth};
 
 Route::get('/', 'Auth\LoginController@index');
 Auth::routes(['register' => false]);
 
-Route::middleware('auth')
+Route::prefix('admin')
+    ->middleware('auth')
     ->group(function () {
         Route::get('/home', 'HomeController@index')->name('home');
     });
 
-Route::middleware('ketua')
+Route::prefix('ketua')
+    ->middleware('ketua')
     ->group(function () {
         Route::get('/private', function () {
             return 'Halaman Ketua';
