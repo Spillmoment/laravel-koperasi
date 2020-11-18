da721c2fb43e6e86e0209ef1794524f6f1e8c814
gunawan98 <deddygunawan98@gmail.com>
2020-11-18 17:56:45 +0700

    create model jenis_simpanan

---
+++ app/JenisSimpanan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class JenisSimpanan extends Model
+{
+    protected $table = 'jenis_simpanan';
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['nama_simpanan','minimal_simpan'];
+
+    public function simpanan()
+    {
+        return $this->hasMany(Simpanan::class, 'jenis_simpanan_id', 'id');
+    }
+}
