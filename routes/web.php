fe853f79e68f36b004934856844d37bf5d0f7808
gunawan98 <deddygunawan98@gmail.com>
2020-12-26 09:30:26 +0700

    add route rekap per anggota

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::prefix('admin')
         Route::get('pengaturan', 'Admin\DashboardController@pengaturan')->name('admin.pengaturan');
         Route::put('update-pengaturan/{user}', 'Admin\DashboardController@update_pengaturan')->name('admin.update-pengaturan');
         Route::resource('simpanan', 'SimpananController');
+        Route::get('/rekap/anggota', 'SimpananController@anggota')->name('simpanan.anggota');
     });
 
 Route::prefix('ketua')
