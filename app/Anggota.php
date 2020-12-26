8c060d311647acbe53e77c6067c3905d8726457a
Hafet17 <do.crazy192@gmail.com>
2020-12-26 23:12:36 +0700

    fixing model anggota

--- app/Anggota.php
+++ app/Anggota.php
@@ -10,16 +10,21 @@ class Anggota extends Model
     protected $table = 'anggota';
     protected $primaryKey = 'id';
 
-    protected $fillable = ['no_ktp','nama_anggota', 'jenis_kelamin', 'alamat', 'kota', 'telepon', 'pengurus'];
+    protected $fillable = ['no_ktp', 'nama_anggota', 'jenis_kelamin', 'alamat', 'kota', 'telepon', 'pengurus'];
     use SoftDeletes;
 
     public function simpanan()
     {
         return $this->hasMany(Simpanan::class, 'anggota_id', 'id');
     }
-    
+
     public function pinjaman()
     {
-        return $this->hasMany(Simpanan::class, 'anggota_id', 'id');
+        return $this->hasMany(Pinjaman::class, 'anggota_id', 'id');
+    }
+
+    public function count()
+    {
+        return $this->hasMany(Count::class, 'anggota_id', 'id');
     }
 }
