75e9b423d63bb6e065668828128094014a48496f
Hafet17 <do.crazy192@gmail.com>
2020-12-26 23:11:31 +0700

    add DataTable in AdminController

--- app/Http/Controllers/Ketua/AdminController.php
+++ app/Http/Controllers/Ketua/AdminController.php
@@ -7,35 +7,63 @@ use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\{Storage, Hash};
 use Illuminate\Support\Arr;
+use Yajra\DataTables\Facades\DataTables;
 
 class AdminController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        return view('ketua.user.index', ['users' => User::paginate(12)]);
+        if (request()->ajax()) {
+            $query = User::query();
+
+            return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                        <div class="btn-group">
+                            <div class="dropdown">
+                                <button class="btn btn-primary dropdown-toggle mr-1 mb-1" 
+                                    type="button" id="action' .  $item->id . '"
+                                        data-toggle="dropdown" 
+                                        aria-haspopup="true"
+                                        aria-expanded="false">
+                                        Aksi
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="action' .  $item->id . '">
+                                    <a class="dropdown-item" href="' . route('admin.edit', $item->id) . '">
+                                        Sunting
+                                    </a>
+                                    <form action="' . route('admin.destroy', $item->id) . '" method="POST">
+                                        ' . method_field('delete') . csrf_field() . '
+                                        <button type="submit" class="dropdown-item text-danger">
+                                            Hapus
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                    </div>';
+                })
+                ->editColumn('image', function ($item) {
+                    return $item->image ? '<img src="' . Storage::url($item->image) . '" style="max-height: 40px;"/>' : '';
+                })
+                ->editColumn('roles', function ($item) {
+                    return $item->roles == 'admin'
+                        ? '<span class="text-warning">' . $item->roles . '</span>'
+                        : '<span class="text-success">' . $item->roles . '</span>';
+                })
+                ->rawColumns(['action', 'image', 'roles'])
+                ->make();
+        }
+
+        return view('ketua.user.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('ketua.user.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $request->validate([
@@ -56,36 +84,18 @@ class AdminController extends Controller
             ->with(['status' => 'Data User Berhasil Ditambahkan']);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $user = User::findOrFail($id);
         return view('ketua.user.show', compact('user'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
 
@@ -117,20 +127,14 @@ class AdminController extends Controller
         }
 
         $user->update($data);
-        return redirect()->route('admin.index')->with(['success' => 'Data User' . $user->name . 'Berhasil Diubah']);
+        return redirect()->route('admin.index')->with(['status' => 'Data User ' . $user->name . '  Berhasil Diubah']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $user = User::findOrFail($id);
         Storage::delete('public/' . $user->image);
         $user->delete();
-        return redirect()->route('admin.index')->with(['success' => 'Data User' . $user->name . 'Berhasil Dihapus']);
+        return redirect()->route('admin.index')->with(['status' => 'Data User ' . $user->name . ' Berhasil Dihapus']);
     }
 }
