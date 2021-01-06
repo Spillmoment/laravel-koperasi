2b25344cc7aefc38acef1455710ef034582fa067
gunawan98 <deddygunawan98@gmail.com>
2021-01-06 08:12:23 +0700

    edit simpanan anggota blade

--- resources/views/member/simpanan/simpanan_anggota.blade.php
+++ resources/views/member/simpanan/simpanan_anggota.blade.php
@@ -118,7 +118,7 @@
                                     @foreach ($simpanan_3 as $simp3)
                                     <tr>
                                         <td>{{ $simp3->created_at->format('j F Y') }}</td>
-                                        <td>{{ $simp3->nominal }}</td>
+                                        <td class="{{ $simp3->nominal < 0 ? 'text-danger' : '' }}">{{ $simp3->nominal }}</td>
                                     </tr>                                            
                                     @endforeach
                                     <tr>
