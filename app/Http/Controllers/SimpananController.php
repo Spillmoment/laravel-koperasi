680f904ae27c9ef39700c20a61d949e92282bd0d
gunawan98 <deddygunawan98@gmail.com>
2021-01-06 08:13:02 +0700

    simpanan controler add penarikan func

--- app/Http/Controllers/SimpananController.php
+++ app/Http/Controllers/SimpananController.php
@@ -53,7 +53,7 @@ class SimpananController extends Controller
 
         Simpanan::create($data);
 
-        return redirect()->route('simpanan.create')->with(['status' => 'Data Simpanan Berhasil Ditambahkan']);
+        return redirect('admin/simpanan')->with(['success' => 'Data Simpanan Berhasil Ditambahkan']);
     }
 
     /**
@@ -75,7 +75,9 @@ class SimpananController extends Controller
      */
     public function edit(Simpanan $simpanan)
     {
-        //
+        $data_relasi = Simpanan::with(['anggota','jenis_simpanan'])->where('id', $simpanan->id)->first();
+        $data_jenis_simpanan = JenisSimpanan::all();
+        return view('member.simpanan.simpanan_edit', compact('simpanan', 'data_relasi', 'data_jenis_simpanan'));
     }
 
     /**
@@ -87,7 +89,19 @@ class SimpananController extends Controller
      */
     public function update(Request $request, Simpanan $simpanan)
     {
-        //
+        $request->validate([
+            'jenis_simpanan_id' => 'required|exists:jenis_simpanan,id',
+            'nominal' => 'required|numeric',
+            'keterangan' => 'max:200',
+        ]);
+
+        Simpanan::where('id', $simpanan->id)->update([
+            'jenis_simpanan_id' => $request->jenis_simpanan_id,
+            'nominal' => $request->nominal,
+            'keterangan' => $request->keterangan,
+        ]);
+
+        return redirect('admin/simpanan')->with(['success' => 'Data Berhasil Diupdate.']);
     }
 
     /**
@@ -98,7 +112,9 @@ class SimpananController extends Controller
      */
     public function destroy(Simpanan $simpanan)
     {
-        //
+        $simpanan = Simpanan::findOrFail($simpanan->id);
+        $simpanan->forceDelete();
+        return redirect('admin/simpanan')->with(['success' => 'Data simpanan ID '.$simpanan->id.' Berhasil Dihapus']);
     }
 
     public function anggota()
@@ -122,5 +138,38 @@ class SimpananController extends Controller
         // dd($count_simpanan_3);
 		return view('member.simpanan.simpanan_anggota', compact('anggota','data_simpanan', 'simpanan_1', 'count_simpanan_1', 'simpanan_2', 'count_simpanan_2', 'simpanan_3', 'count_simpanan_3'));
  
-	}
+    }
+    
+    public function penarikan_simpanan()
+    {
+        // dd('tes');
+        $data_anggota = Anggota::all();
+        $data_jenis_simpanan = JenisSimpanan::all();
+        return view('member.simpanan.simpanan_penarikan', compact('data_anggota', 'data_jenis_simpanan'));
+    }
+
+    public function penarikan_post(Request $request)
+    {
+        $request->validate([
+            'anggota_id' => 'required|exists:anggota,id',
+            'nominal' => 'required|numeric',
+            'keterangan' => 'max:200',
+        ]);
+
+        $cek_jumlah = Simpanan::select(DB::raw('SUM(nominal) as total_simpanan'))->where('anggota_id', $request->anggota_id)->where('jenis_simpanan_id', 3)->first();
+        $saldo = $cek_jumlah->total_simpanan;
+        $ambil = $request->nominal;
+        if( $saldo < $ambil ){
+            return redirect()->route('simpanan.penarikan')->with(['error' => 'Saldo simpanan sukarela Anda tidak cukup.']);
+        } else {
+            $simpanan = Simpanan::create([
+                'anggota_id' => $request->anggota_id,
+                'jenis_simpanan_id' => 3,
+                'nominal' => '-'.$request->nominal,
+                'keterangan' => $request->keterangan,
+            ]);
+    
+            return redirect()->route('simpanan.penarikan')->with(['success' => 'Penarikan simpanan berhasil.']);
+        }
+    }
 }
